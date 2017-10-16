bf4b4ddd669083723f63ef3b19719bca411d00d6
kayo <kayo@Kayo-2.local>
2017-10-16 18:02:34 +0800

    commit change ${commitTime}

--- commit.sh
+++ commit.sh
@@ -1,12 +1,15 @@
 filePath=$0
-echo "当前的命令路径：" $filePath
+#echo "当前的命令路径：" $filePath
 echo "--------开始执行 pull---------"
 git pull
 echo "--------开始执行 add .---------"
 git add .
 echo "--------开始执行 commit---------"
-git commit -m 'commit change'
+commitTime= date +%F..%H:%M:%S
+git commit -m 'commit change ${commitTime}'
 git status
 echo "--------开始执行 push---------"
-git push
+#git push
+
+
 
