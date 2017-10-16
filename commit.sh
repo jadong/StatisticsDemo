1be9ae6180e8ae7581f7307e068daf9f3419c6a2
kayo <kayo@Kayo-2.local>
2017-10-16 18:10:47 +0800

    commit change  time==>>2017-10-16..18:10:47

--- commit.sh
+++ commit.sh
@@ -6,10 +6,14 @@ echo "--------开始执行 add .---------"
 git add .
 echo "--------开始执行 commit---------"
 commitTime=$(date "+%F..%H:%M:%S")
-git commit -m "commit change $commitTime"
+git commit -m "commit change  time==>>$commitTime"
 git status
 echo "--------开始执行 push---------"
-#git push
+git push
+echo "--------查看提交状态---------"
+git status
+echo "--------代码操作结束---------"
+
 
 
 
