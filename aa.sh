1865bcc4114c805ddbcc8a76f50227e0ed60f8c5
kayo <kayo@Kayo-2.local>
2017-10-16 21:07:17 +0800

    add code

--- aa.sh
+++ aa.sh
@@ -1,8 +1,19 @@
 filePath=$0
 echo "-----------------------------------"
+exit_script(){
+    echo "退出j脚本"
+    exit 1
+}
 aaa=$(git "pull")
-bbb="Already"
+#aaa="Unpacking objects: 100%"
+bbb1="Already up-to-date."
+bbb2="Unpacking objects: 100%"
 echo "数据$aaa"
-if  [[ $aaa =~ $bbb ]];then
-    echo "包含$aaa"
+if  [[ $aaa =~ $bbb1 ]] || [[ $aaa =~ $bbb2 ]];then
+    echo "拉取代码成功"
+else
+    echo "拉取代码成功失败"
+    exit_script
 fi
+
+
