045695610799d73c840a942e779c72ccd7f2e04f
kayo <kayo@Kayo-2.local>
2017-10-16 17:52:06 +0800

    commit change

--- commit.sh
+++ commit.sh
@@ -1,9 +1,9 @@
 filePath=$0
 echo "当前的命令路径：" $filePath
-echo "开始执行BUILD"
-#gradle uploadArchives
-localPathaa = 'pwd'
-echo " " $localPathaa
+echo "--------开始执行 pull---------"
 git pull
+echo "--------开始执行 add .---------"
 git add .
+echo "--------开始执行 commit---------"
 git commit -m 'commit change'
+git status
