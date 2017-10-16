6fb164182b41be1d2c482a7c43a8ad7851138b13
kayo <kayo@Kayo-2.local>
2017-10-16 22:16:06 +0800

    commit change  time==>>2017-10-16..22:16:06

--- aa.sh
+++ aa.sh
@@ -1,16 +1,31 @@
 filePath=$0
 
 log(){
-    echo "-----------------------------------"
-    echo "-----$1-----"
-    echo "-----------------------------------"
+    l1="-----------------------------------"
+    l2="-----"
+    if [ $1 = "i" ];then
+        echo  "$l1"
+        echo  "$l2$2$l2"
+        echo  "$l1"
+        pass
+    elif [ $1='w' ];then
+        echo  "\033[31m$l1\033[0m"
+        echo  "\033[31m$l2$2$l2\033[0m"
+        echo  "\033[31m$l1\033[0m"
+    fi
+}
+info(){
+    log "i" $1
+}
+warn(){
+    log "w" $1
 }
 exit_script(){
-    echo "退出j脚本"
+    warn "退出j脚本"
     exit 1
 }
 normal="nothing to commit, working tree clean"
-log "执行 pull"
+info "执行 pull"
 
 aaa=$(git "pull")
 #aaa="Unpacking objects: 100%"
@@ -24,10 +39,10 @@ else
     exit_script
 fi
 #添加修改的文件
-log "执行 add . 添加更改文件"
+info "执行 add . 添加更改文件"
 git add .
 
-log "执行 commit"
+info "执行 commit"
 commitTime=$(date "+%F..%H:%M:%S")
 ccc=$(git commit -m "commit change  time==>>$commitTime")
 ccc2="no changes added to commit"
@@ -46,7 +61,7 @@ fi
 sss1=$normal
 sss2="Changes to be committed"
 function check_status(){
-    log "开始验证提交状态"
+    info "开始验证提交状态"
     sss=$(git "status")
     if [[ $sss =~ $sss1 ]]; then
         echo "当前状态无可提交数据"
@@ -56,10 +71,9 @@ function check_status(){
     fi
 }
 #push代码
-log "执行 push"
-ppp1="Compressing objects: 100%"
-ppp=$(git "push")
-echo $ppp
+warn "执行 push"
+git push
+echo "-----代码推送完成-----"
 check_status
 
 
